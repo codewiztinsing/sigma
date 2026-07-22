17c6cc37f402bb73b51fdaa081871170b1c939ba
tinsae <tinsaealako@gmail.com>
2026-07-22 10:38:51 +0300

    commit

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get(
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+DEBUG = True #os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = ['sigma-izip.onrender.com', 'sigma-xo47.onrender.com']
 if DEBUG:
